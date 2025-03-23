104d2c78f3271a1b94d926007e70031a56a10d76
Yashshri-22 <yashshri2205@gmail.com>
2025-03-23 12:36:46 +0530

    Home Page Designed

---
+++ home/home.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bannerButton = document.getElementById("new_registration");
+
+    const newRegistrationCard = document.getElementById("option1");
+    const disabledLoginCard = document.getElementById("option2");
+    const infoCard = document.getElementById("option3");
+    const donateCard = document.getElementById("option4");
+
+    const registerDown = document.querySelector(".register-btn");
+
+    if (bannerButton) {
+        console.log("Button found:", bannerButton);
+
+        bannerButton.addEventListener("click", function () {
+            window.location.href = "../Login/register.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+    if (newRegistrationCard) {
+        console.log("Button found:", newRegistrationCard);
+
+        newRegistrationCard.addEventListener("click", function () {
+            window.location.href = "../Login/register.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+    if (disabledLoginCard) {
+        console.log("Button found:", disabledLoginCard);
+
+        disabledLoginCard.addEventListener("click", function () {
+            window.location.href = "../Login/login.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+    if (infoCard) {
+        console.log("Button found:", infoCard);
+
+        infoCard.addEventListener("click", function () {
+            window.location.href = "../Login/register.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+    if (donateCard) {
+        console.log("Button found:", donateCard);
+
+        donateCard.addEventListener("click", function () {
+            window.location.href = "../Login/register.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+    if (registerDown) {
+        console.log("Button found:", registerDown);
+
+        registerDown.addEventListener("click", function () {
+            window.location.href = "../Login/register.html";
+        });
+    } else {
+        console.error("Button not found!");
+    }
+
+});
